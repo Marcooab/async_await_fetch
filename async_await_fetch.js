e88505bfa765458e68f4ca82711961fd838bfc84
Marco Aurelio Ricardo <102860659+Marcooab@users.noreply.github.com>
2022-09-23 21:49:07 +0100

    Update async_await_fetch.js

--- async_await_fetch.js
+++ async_await_fetch.js
@@ -1,17 +1,11 @@
 function getUser(id) { 
-
     return fetch (`https://reqres.in/api/users?id=${id}`) 
-
     .then ((data) => data.json()) 
-
     .catch ((err) => console.log (err)) 
 }
 
-
 async function showUserName(id) {
-
     const user = await getUser(id) 
-
     console.log(`o nome do usuário é: ${user.data.last_name}`) 
 }
 
