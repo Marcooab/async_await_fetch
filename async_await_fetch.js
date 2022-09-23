ad24d72b6471a512f68f91cd5168380cc31cafdd
Marco Aurelio Ricardo <102860659+Marcooab@users.noreply.github.com>
2022-09-23 21:48:48 +0100

    Update async_await_fetch.js

--- async_await_fetch.js
+++ async_await_fetch.js
@@ -1,21 +1,18 @@
-// Exemplo de Funcao Async Await (Fetch) na busca do último nome de usuario. 
+function getUser(id) { 
 
-// Funcao p/ pegar os dados desejados
-function getUser(id) { // ID é usado como argumento para busca do usuario 
+    return fetch (`https://reqres.in/api/users?id=${id}`) 
 
-    return fetch (`https://reqres.in/api/users?id=${id}`) // URL da API onde os dados serao extraidos e a indicacao de que o ID é o parametro para definir qual usuario 
+    .then ((data) => data.json()) 
 
-    .then ((data) => data.json()) // Request com pedido de arquivo no tipo .json
-
-    .catch ((err) => console.log (err)) // Catch para exibicao de possivel erro com a request
+    .catch ((err) => console.log (err)) 
 }
 
-// Funcao para exibicao dos dados coletados pela funcao getUser
+
 async function showUserName(id) {
 
-    const user = await getUser(id) // Await com finalidade de fazer com que a funcao aguarde ate que a outra funcao (getUSer) seja executada e forneca os dados que serao exibidos, agora sim, por esta funcao (showUserName)
+    const user = await getUser(id) 
 
-    console.log(`o nome do usuário é: ${user.data.last_name}`) // Indicacao de qual dado deve ser exibido. Neste caso o ultimo nome.
+    console.log(`o nome do usuário é: ${user.data.last_name}`) 
 }
 
-showUserName(1); // Chamada da Funcao com indicacao do parametro do usuario desejado(1)
+showUserName(1); 
